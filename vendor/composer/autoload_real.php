c02181b3647daf8c35bad73d1b37fc377c776cab
Marco Amaya <marcoamayalizcano@gmail.com>
2020-10-24 21:23:43 -0400

    final modules add

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInit9e98581fd67d5870d3ff87e054f00a0d
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
